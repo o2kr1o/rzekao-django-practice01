77adbb9ef32c163106efdb39f82ce4f4e0c5c447
o2kr1o <o2kr1o@yahoo.co.jp>
2022-03-08 08:45:37 +0000

    first commit

---
+++ website/views.py
@@ -0,0 +1,23 @@
+from django.views.generic import TemplateView
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+    
+    def get_context_data(self):
+        ctxt = super().get_context_data()
+        ctxt["username"] = ""
+        return ctxt
+    
+class AboutView(TemplateView):
+    template_name = "about.html"
+    
+    def get_context_data(self):
+        ctxt = super().get_context_data()
+        ctxt["num_services"] = 1000
+        ctxt["skills"] = [
+            "Python",
+            "Javascript",
+            "MySQL",
+            ]
+        return ctxt
+    
